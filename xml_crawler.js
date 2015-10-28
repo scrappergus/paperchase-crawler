18d128298346e86447de3acae72bfe981de9f210
jackson b <jacksonbrsrd@gmail.com>
2015-10-28 06:16:48 -0700

    Now storing XML in S3 and Mongo. Modularity improved, works for all journals.

--- xml_crawler.js
+++ xml_crawler.js
@@ -1,98 +1,45 @@
 var config = require('./config');
 var fs = require('fs');
+var s3 = require('s3');
 var async = require('async');
 var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
 var xml2js = require('xml2js');
-var JSFtp = require("jsftp");
 var request = require('request');
-
-var express = require('express');
-var api = express();
 
 var dbURL = 'mongodb://localhost:27017/paperchase';
 
-var ISSN_list = ["1945-4589", "", "", ""];
+var ISSN_list = [{journal: "aging", issn:"1945-4589"},
+				 {journal: "oncotarget", issn:"1949-2553"},
+				 {journal: "oncoscience", issn:" 2331-4737"},
+				 {journal: "genesandcancer", issn:"1947-6027"}];
 
-function get_xml_string_with_added_doi(xml_info, cb) {
-	request(xml_info.url, function(err, res, body) {
-		if(!err) {
-			xml2js.parseString(body, function(parse_err, parse_res) {
-				var doi = "10.18632/oncotarget."+xml_info.pii;
-				parse_res.PubmedArticleSet.PubmedArticle[0].PubmedData[0].ArticleIdList[0].ArticleId.push({"_":doi, "$":{"IdType": "doi"}});
-				var builder = new xml2js.Builder();
-				var xml = builder.buildObject(parse_res);
-				cb(null, xml);
-			});
-		} else {
-			cb(err);
-		}
+var s3Client = s3.createClient({
+	s3Options: {
+		accessKeyId: config.s3.key,
+		secretAccessKey: config.s3.secret
+	}
+});
+
+function get_xml_string_from_url(xml_url, cb) {
+	request(xml_url, function(err, res, body) {
+		cb(null, body);
 	});
 }
 
-function convert_article_title_to_uid(title_string, cb) {
-	var journal_name = "oncotarget";
-	var pmid_query_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term="+encodeURIComponent(title_string)+"+"+journal_name+"&RetMax=1";
-	var pmcid_query_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term="+encodeURIComponent(title_string)+"+"+journal_name+"&RetMax=1";
-	get_uid_from_esearch_url(pmid_query_url, function(err, res){
-		if(err) {
-			get_uid_from_esearch_url(pmcid_query_url, function(err_pmc, res_pmc) {
-				if(err_pmc) cb(err_pmc);
-				else cb(null, {"type": "pmcid", "uid": res_pmc});
-			});
-		} else {
-			cb(null, {"type": "pmid", "uid": res});
-		}
+function get_idlist_from_abstract_xml(xml_string, cb) {
+	xml2js.parseString(xml_string, function(parse_err, parse_res) {
+		var idlist = parse_res.PubmedArticleSet.PubmedArticle[0].PubmedData[0].ArticleIdList[0].ArticleId;
+		massage_articleid_list(idlist, cb);
 	});
 }
 
-function uid_list_from_pii_list(pii_list, cb) {
-	var pii_list_string = pii_list.join(",");
-	var article_json_url = config.doicontrol_url+"/doi-control/?journal=oncotarget&pii="+pii_list_string;
-	request(article_json_url, function(err, res, body) {
-		if(err){
-			cb(err);
-		} else if (res.statusCode != 200) {
-			cb({"error": "There was an error getting the article JSON when converting PIIs to PMIDs", "body": body});
-		} else {
-			var articles = JSON.parse(body)["articles"];
-			async.mapSeries(articles, function(article, map_cb){
-				convert_article_title_to_uid(article.title, function(err,res) {
-					res.pii = article.pii;
-					map_cb(err, res);
-			});
-			}, cb);
-		}
+function massage_articleid_list(idlist, cb) {
+	async.map(idlist, function(id, map_cb){
+		map_cb(null, {type: id.$.IdType, id: id._});
+	}, function(err, res){
+		cb(null, res);
 	});
-}
-
-function get_uid_from_esearch_url(url, cb) {
-	request(url, function(err, res, body){
-		if(err) {
-			cb(err);
-		} else {
-			xml2js.parseString(body, function(parse_err, parse_res) {
-				if(parse_err) {
-					cb(parse_err);
-				}
-				else {
-					var pmid_list = parse_res.eSearchResult.IdList[0].Id;
-					if(pmid_list != void 0 && pmid_list.length > 0) cb(null, pmid_list[0]);
-					else cb({"error": "There was no UID for url: "+url});
-				}
-			});
-		}
-	});
-}
-
-function uid_list_to_xml_url_list(uid_list, cb) {
-	async.mapSeries(uid_list, function(uid, map_cb) {
-		var url_bases = {
-			"pmid": "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pubmed&report=xml&id=",
-			"pmcid": "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pmc&report=xml&id=PMC"
-		};
-		map_cb(null, {"pii": uid.pii, "type": uid.type, "uid": uid.uid, "url": url_bases[uid.type]+uid.uid});
-	}, cb);
 }
 
 function get_pmid_list_for_issn(issn, cb) {
@@ -114,42 +61,118 @@ function get_pmid_list_for_issn(issn, cb) {
 	});
 }
 
-function upload_xml_string_as_file_to_ftp(xml_string, xml_filename, cb) {
-	var ftp = new JSFtp({
-		host: config.ftp.host,
-		user: config.ftp.user,
-		pass: config.ftp.pass
-	});
-	var xml_buffer = new Buffer(xml_string);
-	ftp.put(xml_buffer, xml_filename, function(err){
-		if(err) cb(err)
-		else cb(null, "success!");
-	});
-}
-
-api.get('/add-doi/:piis', function(req, response) {
-	var pii_list = req.params.piis.split(",");
-	uid_list_from_pii_list(pii_list, function(err, res){
+function upload_xml_string_as_file_to_s3(xml_string, xml_filename, cb) {
+	var tempdir = "./temp";
+	if (!fs.existsSync(tempdir)){
+		fs.mkdirSync(tempdir);
+	}
+	var xml_filepath = tempdir + "/" + xml_filename;
+	fs.writeFile(xml_filepath, xml_string, function(err){
 		if(err) {
-			console.error(err);
+			cb(err);
 		} else {
-			uid_list_to_xml_url_list(res, function(url_err, url_res){
-				async.eachSeries(url_res, function(cur_url, each_cb) {
-					get_xml_string_with_added_doi(cur_url, function(xml_err, xml_res){
-						upload_xml_string_as_file_to_ftp(xml_res, "oncotarget_"+cur_url.uid+".xml", each_cb);
-					});
-				}, function(each_err, each_res) {
-					var data = {};
-					if(each_err) data = {"error": each_err};
-					else data = {"success": true};
-					response.setHeader('Content-Type', 'application/json');
-					response.send(JSON.stringify(data));
-				});
-
+			var uploader = s3Client.uploadFile({localFile: xml_filepath, s3Params: {Bucket: config.s3.bucket, Key: xml_filename}});
+			uploader.on("error", function(err) {cb(err)});
+			uploader.on("progress", function() {console.log(xml_filename+" progress:", uploader.progressTotal)});
+			uploader.on("end", function() {
+				var s3url = s3.getPublicUrlHttp(config.s3.bucket, xml_filename);
+				cb(null, s3url);
+				fs.unlink(xml_filepath);
 			});
 		}
 	});
-});
+}
 
-api.listen(4933);
-console.log("Server started");
+function get_pmcid_from_idlist(idlist, cb) {
+	async.filterSeries(idlist, function(id, filter_cb){
+		console.log(id);
+		filter_cb((id.type == "pmc"));
+	}, function(res){
+		if(res.length < 1) cb("no PMCID, skipping");
+		else cb(null, res[0].id);
+	});
+}
+
+function get_idlist_s3url_pair_from_pmid(pmid, cb) {
+	var abstract_xml_filename = Date.now()+"_"+pmid+".xml";
+	var abstract_xml_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pubmed&report=xml&id="+pmid;
+	get_xml_string_from_url(abstract_xml_url, function(abstract_xml_err, abstract_xml_body){
+		if(abstract_xml_err) {
+			cb(abstract_xml_err);
+		} else {
+			get_idlist_from_abstract_xml(abstract_xml_body, function(idlist_err, idlist) {
+				if(idlist_err) {
+					cb(idlist_err);
+				} else {
+					get_pmcid_from_idlist(idlist, function(pmcid_err, pmcid){
+						if(pmcid_err) {
+							upload_xml_string_as_file_to_s3(abstract_xml_body, abstract_xml_filename, function(err, s3url){
+								if(err) { cb(err); }
+								else {
+									var pair = {ids: idlist, abstract_xml_url: s3url};
+									cb(null, pair);
+								}
+							});
+						} else {
+							var full_xml_filename = Date.now()+"_"+pmcid+".xml";
+							var full_xml_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pmc&report=xml&id="+pmcid;
+							get_xml_string_from_url(full_xml_url, function(full_xml_err, full_xml_body){
+								async.series([
+									function(scb) {
+										upload_xml_string_as_file_to_s3(abstract_xml_body, abstract_xml_filename, scb);
+									},
+									function(scb) {
+										upload_xml_string_as_file_to_s3(full_xml_body, full_xml_filename, scb);
+									}
+								],function(series_err, series_res){
+									if(series_err) { cb(series_err); }
+									else {
+										var pair = {ids: idlist, abstract_xml_url: series_res[0], full_xml_url: series_res[1]};
+										cb(null, pair);
+									}
+								});
+							});
+						}
+					});
+				}
+			});
+		}
+	});
+}
+
+MongoClient.connect(dbURL, function(err, db) {
+	async.eachSeries(ISSN_list, function(current_issn_data, each_issn_cb){
+		var current_issn = current_issn_data.issn;
+		var current_journal = current_issn_data.journal;
+		var current_db_collection = db.collection(current_journal+"_xml");
+		get_pmid_list_for_issn(current_issn, function(err, list){
+			async.mapSeries(list, function(pmid, map_cb){
+				console.log("start processing: " + pmid);
+				get_idlist_s3url_pair_from_pmid(pmid, function(pair_err, pair_res){
+					if(pair_err) { console.error(pair_err); map_cb(); }
+					else { map_cb(null, pair_res); }
+				});
+			}, function(err, journal_stored_xml_list){
+				if(err) {
+					console.error(err);
+				} else {
+					async.each(journal_stored_xml_list, function(o, each_xml_cb){
+						if(!o) {
+							each_xml_cb();
+						} else {
+							current_db_collection.update({ids: {"$in": o.ids}}, o, {upsert: true});
+							console.log(o);
+							console.log("stored!");
+							each_xml_cb();
+						}
+					}, function() {
+						each_issn_cb();
+					});
+				}
+			});
+		});
+	}, function() {
+		console.log("done!");
+		db.close();
+	});
+});
