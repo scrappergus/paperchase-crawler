7207658dc91c79908bccf9dd9323a0213c71c228
jackson b <jacksonbrsrd@gmail.com>
2015-10-29 10:44:10 -0700

    route to combine figures with xml

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var async = require('async');
 var request = require('request');
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
@@ -39,6 +40,26 @@ function get_figures_by_pii(journal_name, pii, cb) {
 	mongo_query(collection_name, query, cb);
 }
 
+function get_xml_with_figures_by_pii(journal_name, pii, cb) {
+	var query = {"ids": {"type": "pii", "id": pii}};
+	async.waterfall([
+		function(wcb) {
+			var collection_name = journal_name+"_xml";
+			mongo_query(collection_name, query, wcb);
+		},
+		function(xml_data, wcb) {
+			var collection_name = journal_name+"_figures";
+			mongo_query(collection_name, query, function(err, res) {
+				if(err) { wcb(err); return; }
+				if(res.length < 1) { wcb(null, xml_data); return; }
+				xml_data.figures = res[0].figures;
+				console.log(res);
+				wcb(null, xml_data);
+			});
+		}
+	], cb);
+}
+
 app.get('/fetchxml/:journalname', function(req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	var journal_name = req.params.journalname;
@@ -69,6 +90,19 @@ app.get('/fetchfigures/:journalname/pii/:pii', function(req, res) {
 	var journal_name = req.params.journalname;
 	var pii = req.params.pii;
 	get_figures_by_pii(journal_name, pii, function(xml_err, xml_res) {
+		if(xml_err) {
+			res.status(500).send(JSON.stringify(xml_err));
+		} else {
+			res.send(JSON.stringify(xml_res));
+		}
+	});
+});
+
+app.get('/xmlfigures/:journalname/pii/:pii', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	var journal_name = req.params.journalname;
+	var pii = req.params.pii;
+	get_xml_with_figures_by_pii(journal_name, pii, function(xml_err, xml_res) {
 		if(xml_err) {
 			res.status(500).send(JSON.stringify(xml_err));
 		} else {
