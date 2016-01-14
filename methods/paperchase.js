e6fe7eb29cae3cbeebfa20091b15b220e27d1fea
Jessica Lawshe <jessica@jlawshe.com>
2016-01-14 15:06:23 -0600

    Insert into paperchase DB using credentials

--- methods/paperchase.js
+++ methods/paperchase.js
@@ -12,9 +12,9 @@ var journalSettings = config.journalSettings;
 var paperchase = {};
 
 paperchase.articleCount = function(journal,cb){
+	// console.log('...articleCount : ' + journal);
 	var dbUrl = journalSettings[journal].dbUrl;
 	if(config.live){
-		console.log('...articleCount : ' + journal);
 		var dbName = journalSettings[journal]['mongo']['name'];
 		var dbUser = journalSettings[journal]['mongo']['user'];
 		var dbPw = journalSettings[journal]['mongo']['password'];
@@ -25,7 +25,6 @@ paperchase.articleCount = function(journal,cb){
 		var db = new Db(dbName, new Server(dbServer, dbPort));
 		// Establish connection to db
 		db.open(function(err, db) {
-		    // assert.equal(null, err);
 		    if(err){
 		    	console.error('DB connection ERROR',err);
 		    }else{
@@ -37,12 +36,12 @@ paperchase.articleCount = function(journal,cb){
 						// console.log('USER authenticated');
 						var articleCount = 0 ;
 						db.collection('articles').find({},{_id:1}).toArray(function(articlesCountErr, articles){
-							// do something with items
 							if(articlesCountErr){
 								console.error('Article Count ERROR',articlesCountErr);
 								cb(articlesCountErr);
 							}else if(articles){
-								cb(null,articles);
+								console.log(articles.length);
+								cb(null,articles.length);
 							}
 						});
 					}
@@ -56,25 +55,36 @@ paperchase.articleCount = function(journal,cb){
 
 paperchase.insertArticle = function (articleData,journal,cb) {
 	// console.log('..insertArticle');
-	// can do multiple at once.
-	var journalDb = journalSettings[journal].dbUrl;
-	MongoClient.connect(journalDb, function(db_err, db) {
-		if(db_err) {
-			console.error(db_err);
-			cb(db_err);
-			return;
+	var dbName = journalSettings[journal]['mongo']['name'];
+	var dbUser = journalSettings[journal]['mongo']['user'];
+	var dbPw = journalSettings[journal]['mongo']['password'];
+	var dbServer = journalSettings[journal]['mongo']['server'];
+	var dbPort= journalSettings[journal]['mongo']['port'];
+	var dbServer = journalSettings[journal]['mongo']['server'];
+
+	var db = new Db(dbName, new Server(dbServer, dbPort));
+	// Establish connection to db
+	db.open(function(err, db) {
+		if(err){
+			console.error('DB connection ERROR',err);
 		}else{
-			var coll = db.collection('articles');
-			coll.insert(articleData, function(err, docs){
-				if(err){
-					console.error(err);
-				}
-				if(docs){
-					cb(false, docs.insertedCount);
+			// Authenticate
+			db.authenticate(dbUser, dbPw, function(authenticateErr, authenticated) {
+				if(authenticateErr){
+					console.error('DB authenticate ERROR',authenticateErr);
+				}else if(authenticated){
+					// console.log('USER authenticated');
+					var articleCount = 0 ;
+					db.collection('articles').insert(articleData,function(errorInsert,inserted){
+						if(errorInsert){
+							console.error('ERROR inserting', errorInsert);
+						}else if(inserted){
+							cb(null,inserted);
+						}
+					});
 				}
 			});
-		}
-		db.close();
+	    }
 	});
 }
 
@@ -93,7 +103,7 @@ paperchase.allArticlesPii = function(journal,cb){
 	// Establish connection to db
 	db.open(function(err, db) {
 	    if(err){
-	    	console.error(err);
+	    	console.error('DB Connection ERROR. Could not get list of PII in journal',err);
 			cb(err);
 	    }else{
 	      // Authenticate
