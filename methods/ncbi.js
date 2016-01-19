73a5f9b60e1322768f4cbe867b57481b46be3547
Jessica Lawshe <jessica@jlawshe.com>
2016-01-18 18:02:38 -0600

    NCBI method to get Title and IDs via PMID

--- methods/ncbi.js
+++ methods/ncbi.js
@@ -41,7 +41,41 @@ var ncbi = {
 			}
 		});
 	},
+	getTitleAndAllIdsFromPmid: function(pmid,cb){
+		// console.log('..getTitleAndAllIdsFromPmid : ' + pmid );
+		var articleJsonUrl = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=' + pmid;
+		request(articleJsonUrl, function(err, res, body){
+			if(err) {
+				cb(err);
+			}
+			if(res){
+				var articleJson = JSON.parse(res.body);
+					articleJson = articleJson.result[pmid],
+					articleIdList = articleJson['articleids'];
+				var artObj = {};
+					artObj['ids'] = {};
+				for(var i = 0 ; i < articleIdList.length ; i ++){
+					// artObj['ids'][articleIdList[i]['idtype']] = articleIdList[i]['value']; // do not want to get ALL IDs.
+					var idType;
+					idType = articleIdList[i]['idtype'];
+					if(idType == 'pmc'){
+						artObj['ids'][articleIdList[i]['idtype']] = articleIdList[i]['value'];
+					}else if(idType == 'doi'){
+						artObj['ids'][articleIdList[i]['idtype']] = articleIdList[i]['value'];
+					}else if(idType == 'pii'){
+						artObj['ids'][articleIdList[i]['idtype']] = articleIdList[i]['value'];
+					}else if(idType == 'pubmed'){
+						artObj['ids'][articleIdList[i]['idtype']] = articleIdList[i]['value'];
+					}
+				}
+				artObj['title'] = articleJson['title'];
+				// console.log(artObj);
+				cb(null,artObj);
+			}
+		});
+	},
 	allArticlesTitleAndPMID: function(journal,journalSettings,cbBatch){
+		// console.log('..allArticlesTitleAndPMID');
 		// via PubMed
 		var journalDb = journalSettings[journal].dbUrl,
 			journalIssn = journalSettings[journal].issn;
@@ -50,15 +84,14 @@ var ncbi = {
 				console.log('     PubMed Article Count: ' + list.length);
 				// List = All PMID retrieved from PubMed query using Journal ISSN (limit set to 80000 in API request to PubMed DB. updated get_pmid_list_for_issn if archive larger than 80k)
 				async.mapSeries(list, function(pmid, map_cb){
-					// console.log('---- PMID: ' + pmid);
+					console.log('---- PMID: ' + pmid);
 					// Using PMID, retrieve article Title and ID list
 
 					ncbi.getTitleByPMID(pmid, function(articleTitleError, articleTitle){
 						if(articleTitleError) {
 							console.error('ERROR', articleTitleError);
-							map_cb();
-						}
-						if(articleTitle){
+							// map_cb();
+						}else if(articleTitle){
 							var articleObj = {
 								pmid : pmid,
 								title : articleTitle
