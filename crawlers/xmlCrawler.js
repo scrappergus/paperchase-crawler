d05f1712e83cd524a8b27c42188dd85cc907ec30
Jessica Lawshe <jessica@jlawshe.com>
2016-01-20 13:30:07 -0600

    XML Crawler - removed Abstract request to PubMed, using ID list from Paperchase instead

--- crawlers/xmlCrawler.js
+++ crawlers/xmlCrawler.js
@@ -122,22 +122,16 @@ function getPiiFromIdList(idlist, cb) {
 	});
 }
 
-function getAndSavePmcXml(idList, journal, cb){
+function getAndSavePmcXml(paperchaseIdList, journal, cb){
 	// PMC ID and PII exist, now query PMC to get Full Text XML
 	// XML full text filename based on PII.
-	var fullTextXmlFilename;
-	if(idList.pii){
-		fullTextXmlFilename = idList.pii + '.xml';
-	}else if(idList.doi){
-		fullTextXmlFilename = idList.doi + '.xml';
-	}
-	if(fullTextXmlFilename){
-		console.log('     Upload: ' + fullTextXmlFilename);
-		var full_xml_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pmc&report=xml&id=' + idList.pmc;
-		// console.log('    XML: ' + full_xml_url);
+	// paperchaseId = paperchaseId.replace(/\//g,'_');
+	var fullTextXmlFilename = paperchaseIdList.paperchase_id + '.xml';
+	if(paperchaseIdList.pmc){
+		var full_xml_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pmc&report=xml&id=' + paperchaseIdList.pmc;
+		console.log('     Upload: ' + fullTextXmlFilename + '. PMID: ' + paperchaseIdList.pmid + '. PMC XML: ' + full_xml_url);
 		get_xml_string_from_url(full_xml_url, function(full_xml_err, full_xml_body){
 			async.series([
-
 				function(scb) {
 					// Upload Full Text XML
 					upload_xml_string_as_file_to_s3(journal, full_xml_body, fullTextXmlFilename, scb);
@@ -148,79 +142,37 @@ function getAndSavePmcXml(idList, journal, cb){
 					cb(series_err);
 				} else {
 					var pair = {
-						ids: idList,
-						abstract_xml_url: series_res[0],
-						full_xml_url: series_res[1]
+						paperchase_id: paperchaseIdList.paperchase_id,
+						pmid: paperchaseIdList.pmid,
+						full_xml_url: series_res[0]
 					};
-					console.log('      Upload success!');
+					console.log('     Upload success: ' + series_res[0]);
 					// console.log(pair);
 					cb(null, pair);
 				}
 			});
 		});
 	}else{
-		console.log('cannot upload: ' + JSON.stringify(idList));
+		cb(null,null);
 	}
 }
 
 function get_and_save_article_xml(journal, pmid, cb) {
 	// console.log('... get_and_save_article_xml : ' + pmid);
-	// var abstractXmlFilename = 'abstract_' + pmid + '.xml';
-	var abstract_xml_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/?db=pubmed&report=xml&id="+pmid;
-	// Use the PMID to then get the PMC ID, which will then be used below to get the Full Text XML from PMC
-	// Get XML containing article IDs (and abstract)
-	// console.log('     Abstract: ' + abstract_xml_url);
-	get_xml_string_from_url(abstract_xml_url, function(abstract_xml_err, abstract_xml_body){
-		if(abstract_xml_err) {
-			cb(abstract_xml_err);
-		} else {
-			// Parse XML to get aricle ID list
-			get_idlist_from_abstract_xml(abstract_xml_body, function(idList_err, idList) {
-				if(idList_err) {
-					console.error('    ERROR');
-					console.error(idList_err);
-					cb(idList_err);
-				}else if(idList.pmc){
-					if(!idList.pii){
-						// query using PMID for PII in DB
-						paperchase.articlePiiViaPmid(pmid,journal,function(piiError,piiRes){
-							if(piiError){
-								console.error('piiError',piiError);
-							}else if(piiRes){
-								idList.pii = piiRes;
-								// console.log('    pii  = ' + piiRes );
-								getAndSavePmcXml(idList, journal, function(uploadXmlError,uploadXmlRes){
-									if(uploadXmlError){
-										console.error('uploadXmlError',uploadXmlError);
-									}else if(uploadXmlRes){
-										cb(null,uploadXmlRes);
-									}
-								});
-							}else{
-								console.log('     MISSING PII: PMC ID = ' + idList.pmc);
-								cb(null,null);
-							}
-						});
-					}
-					if(idList.pii){
-						getAndSavePmcXml(idList, journal, function(uploadXmlError,uploadXmlRes){
-							if(uploadXmlError){
-								console.error('uploadXmlError',uploadXmlError);
-							}else if(uploadXmlRes){
-								cb(null,uploadXmlRes);
-							}
-						});
-					}
-				}else if(idList.pmc && !idList.pii){
-					cb('     MISSING PII: PMC ID = ' + idList.pmc);
-					// console.log(JSON.stringify(idList));
-					// Filename is based on PII. keep track of articles with a PMC ID (have full text XML) but do not have PII
-				}else{
-					// no PMC ID, cannot get Full Text XML
-					cb('     MISSING PMC ID: PMID ' + idList.pubmed);
-					// console.log(JSON.stringify(idList));
+	paperchase.getArticlePaperchaseIdsViaPmid(pmid,journal,function(paperchaseIdError,paperchaseIdList){
+		if(paperchaseIdError){
+			console.error('paperchaseIdError',paperchaseIdError);
+		}else if(paperchaseIdList){
+			getAndSavePmcXml(paperchaseIdList, journal, function(uploadXmlError,uploadXmlRes){
+				if(uploadXmlError){
+					console.error('uploadXmlError',uploadXmlError);
+				}else if(uploadXmlRes){
+					cb(null,uploadXmlRes);
 				}
 			});
+		}else{
+			console.log('     MISSING Paperchase ID : PMID = ' + pmid);
+			cb(null,null);
 		}
 	});
 }
@@ -241,6 +193,7 @@ module.exports = {
 				// var dbCollection = db.collection('xml');
 				ncbi.get_pmid_list_for_issn(journalIssn, function(err, list){
 					console.log('     Article Count: ' + list.length);
+					// list = ['21779478']; //for local testing
 					// List = All PMID retrieved from PubMed query using Journal ISSN (limit set to 80000 in API request to PubMed DB. updated get_pmid_list_for_issn if archive larger than 80k)
 					async.mapSeries(list, function(pmid, map_cb){
 						console.log('---- PMID: ' + pmid);
@@ -264,8 +217,7 @@ module.exports = {
 							// articlesXmlList = list of all XML uploaded to S3. Contains article IDs and XML URLs
 							// All XML uploaded. Now return the array of articles to Paperchase to then update the DB
 							articlesXmlList = shared.removeEmptyFromArray(articlesXmlList);
-							console.log('articlesXmlList');
-							console.log(articlesXmlList);
+							console.log('articlesXmlList',articlesXmlList);
 							cbBatch(null, articlesXmlList); // remove empty before returning to Paperchase
 						}
 					});
