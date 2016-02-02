99b9dea9742ead161db69e16edf3fa49f28a20f2
Jessica Lawshe <jessica@jlawshe.com>
2016-02-02 12:08:14 -0600

    Removed internal ID
    No longer using paperchase_id, using article doc Mongo ID instead

--- methods/paperchase.js
+++ methods/paperchase.js
@@ -56,6 +56,42 @@ var paperchase = {
 		    }
 		});
 	},
+	// updateAssets: function (journal, assetType, assetData, cb) {
+	// 	console.log('..updateAssets: ' + assetType);
+	// 	// console.log('assetData, ' , assetData);
+	// 	var dbName = journalSettings[journal]['mongo']['name'];
+	// 	var dbUser = journalSettings[journal]['mongo']['user'];
+	// 	var dbPw = journalSettings[journal]['mongo']['password'];
+	// 	var dbServer = journalSettings[journal]['mongo']['server'];
+	// 	var dbPort= journalSettings[journal]['mongo']['port'];
+	// 	var dbServer = journalSettings[journal]['mongo']['server'];
+
+	// 	var db = new Db(dbName, new Server(dbServer, dbPort));
+	// 	// Establish connection to db
+	// 	db.open(function(err, db) {
+	// 		if(err){
+	// 			console.error('DB connection ERROR',err);
+	// 		}else{
+	// 			// Authenticate
+	// 			db.authenticate(dbUser, dbPw, function(authenticateErr, authenticated) {
+	// 				if(authenticateErr){
+	// 					console.error('DB authenticate ERROR',authenticateErr);
+	// 				}else if(authenticated){
+	// 					// console.log('USER authenticated');
+	// 					var articleCount = 0 ;
+	// 					db.collection(assetType).update({'ids.paperchase_id' : assetData.ids.paperchase_id}, assetData, {upsert: true}, function(errorUpdate,updated){
+	// 						if(errorUpdate){
+	// 							console.error('ERROR updating', errorUpdate);
+	// 						}else if(updated){
+	// 							// console.log('updated',updated);
+	// 							cb(null,updated);
+	// 						}
+	// 					});
+	// 				}
+	// 			});
+	// 	    }
+	// 	});
+	// },
 	allArticles: function(journal, query, projection, cb){
 		console.log('...allArticles: ' + journal + ',' , query);
 		var dbUrl = journalSettings[journal].dbUrl;
@@ -73,7 +109,7 @@ var paperchase = {
 		    	console.error('DB Connection ERROR. Could not get list of PII in journal',err);
 				cb(err);
 		    }else{
-		      // Authenticate
+				// Authenticate
 				db.authenticate(dbUser, dbPw, function(authenticateError, userAuthenticated) {
 					if(authenticateError){
 						console.error(authenticateError);
@@ -82,6 +118,7 @@ var paperchase = {
 							if(articlesErr){
 								cb(articlesErr);
 							}else if(articles){
+								// cb(null,articles.slice(0,1)); //local testing
 								cb(null,articles);
 							}else{
 								cb(null,null);
@@ -100,7 +137,8 @@ var paperchase = {
 			}else if(articles){
 				var pairsObject = {};
 				for(var articleIdx=0 ; articleIdx < articles.length ; articleIdx++){
-					if(articles[articleIdx]['ids']['paperchase_id'] && articles[articleIdx]['ids']['pmid']){
+					if(articles[articleIdx].ids.pmid){
+						articles[articleIdx].ids._id = articles[articleIdx]._id;
 						pairsObject[articles[articleIdx]['ids']['pmid']] = articles[articleIdx]['ids'];
 					}
 					if(articleIdx == parseInt(articles.length - 1)){
